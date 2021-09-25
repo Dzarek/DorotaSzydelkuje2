b577f78de6d0024f67b2d9a7ef80312a87c7ef09
Jarek <jarekjanas95@gmail.com>
2021-09-25 19:15:21 +0200

    colors

--- src/pages/orderpages/zabawki/ColorKurier.js
+++ src/pages/orderpages/zabawki/ColorKurier.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../../../styles/ZabawkiPage.css";
-import colors from "../../../images/Products/colors.jpg";
+import colors from "../../../images/Products/colors.png";
 
 const ColorKurier = () => {
   return (
